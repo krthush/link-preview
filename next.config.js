15cbfbb8da81473626a27571f8a9cb8831823c57
krthush <33790334+krthush@users.noreply.github.com>
2021-09-12 16:13:00 +0100

    https://vercel.com/support/articles/how-to-enable-cors#enabling-cors-in-a-next.js-app

--- next.config.js
+++ next.config.js
@@ -1,23 +1,17 @@
+// next.config.js
 module.exports = {
   async headers() {
     return [
       {
-        source: '/api/link-preview',
+        // matching all API routes
+        source: "/api/:path*",
         headers: [
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'Content-Type',
-          },
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'OPTIONS, POST, GET',
-          },
-        ],
-      },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
     ]
-  },
-}
+  }
+};
