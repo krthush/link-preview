24039021b5bffbaa6becb884873b8142ef3bec3c
krthush <33790334+krthush@users.noreply.github.com>
2021-09-12 16:02:37 +0100

    Access-Control-Allow-Methods added to API header for CORS

--- next.config.js
+++ next.config.js
@@ -5,8 +5,16 @@ module.exports = {
         source: '/api/link-preview',
         headers: [
           {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type',
+          },
+          {
             key: 'Access-Control-Allow-Origin',
             value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'OPTIONS, POST, GET',
           },
         ],
       },
